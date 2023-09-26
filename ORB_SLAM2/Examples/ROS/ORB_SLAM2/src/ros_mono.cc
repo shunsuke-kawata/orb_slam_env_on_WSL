6e62c8c091407e91bd92a2d09b374d9873395f5c
shunsuke-kawata <kwt20238120@gmail.com>
2023-09-26 18:39:25 +0900

    delete コメントの削除

--- ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -79,7 +79,6 @@ int main(int argc, char **argv)
 void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
 {
     // Copy the ros image message to cv::Mat.
-    cout<<"ros_mono"<<endl;
     cv_bridge::CvImageConstPtr cv_ptr;
     try
     {
--- ORB_SLAM2/build_ros.sh
+++ ORB_SLAM2/build_ros.sh
@@ -2,6 +2,7 @@ echo "Building ROS nodes"
 
 cd Examples/ROS/ORB_SLAM2
 rm -rf build
+rm -rf build Mono MonoAR RGBD Stereo
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
